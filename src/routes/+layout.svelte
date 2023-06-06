2c0c22b4188ef820a419f94d18f7295ed6657a7b
David Vanderhaar <d.vanderhaarhunter@gmail.com>
2023-06-06 12:20:03 -0500

    header, footer

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { base } from "$app/paths";
+  const year = new Date().getFullYear();
+  const license = "CC BY-NC-SA 4.0";
+</script>
+
+
+<header>
+  <h1>Verse // Verve</h1>
+  <p>A Collection of Collections of Poetry and other Things</p>
+  <nav>
+    <a href="{base}/">Home</a>
+    <a href="{base}/about">About</a>
+    <a href="{base}/contact">Contact</a>
+  </nav>
+</header>
+<slot></slot>
+<footer>
+  <p>by David Vanderhaar - {year} - {license}</p>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,1 @@
-<script>
-  import { base } from "$app/paths";
-</script>
 
-<h1>Verse // Verve</h1>
-<p>A Collection of Collections of Poetry and other Things</p>
-<!-- <p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
-<a href="{base}/about">About</a> -->
