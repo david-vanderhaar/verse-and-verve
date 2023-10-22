ebdffc768c128eea854e292cbc3d611d1bfb10fa
David Vanderhaar <d.vanderhaarhunter@gmail.com>
2023-10-22 17:43:10 -0500

    Updates

---
+++ _app/immutable/chunks/paths.ba861d4f.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_br7teg)==null?void 0:s.base)??"/verse-and-verve";var e;const a=((e=globalThis.__sveltekit_br7teg)==null?void 0:e.assets)??t;export{a,t as b};
