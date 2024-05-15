3caf972ff3eae719b2d873f2bb748e84fe301005
David Vanderhaar <d.vanderhaarhunter@gmail.com>
2024-05-15 15:23:43 -0400

    Updates

--- _app/immutable/chunks/paths.ba861d4f.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_br7teg)==null?void 0:s.base)??"/verse-and-verve";var e;const a=((e=globalThis.__sveltekit_br7teg)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ _app/immutable/nodes/2.d6e435b7.js
@@ -0,0 +1,1 @@
+import{P as m}from"../chunks/2.7dafca12.js";export{m as component};
