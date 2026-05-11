6258a4698656dfd21916af7641faff6d02ada219
david-vanderhaar <d.vanderhaarhunter@gmail.com>
2026-05-10 20:09:35 -0500

    Updates

---
+++ _app/immutable/chunks/page.a323f5c6.js
@@ -0,0 +1,1 @@
+import{W as p}from"./index.7eea47cf.js";import{w as r}from"./paths.9218cf79.js";function n(t,{delay:o=0,duration:a=400,easing:e=p}={}){const s=+getComputedStyle(t).opacity;return{delay:o,duration:a,easing:e,css:i=>`opacity: ${i*s}`}}const m=r(!1);export{n as f,m as p};
